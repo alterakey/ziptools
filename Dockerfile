d889909244aae68797f3b2fa3cd979ba9b518b0e
Takahiro Yoshimura <takahiro_y@monolithworks.co.jp>
2022-11-30 02:28:35 +0900

    Applying current regression

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,6 @@ copy cp932.sh /app/cp932
 copy fixup.sh /app/fixup
 run chmod 700 /app/*
 env PATH /app:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+env TZ Asia/Tokyo
 workdir /out
 cmd ["ls", "-1", "/app/"]
--- cp932.sh
+++ cp932.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
+export LC_ALL=en_US.UTF-8
 fn=$(realpath "$1")
 ofn=${fn/.z/-cp932.z}
 tmp=$(mktemp -d)
 cd $tmp
-env LC_ALL=en_US.UTF-8 unzip $fn
-(env LC_ALL=en_US.UTF-8 find . -name '.DS_Store' -o -name '.git*' -print0 | env LC_ALL=en_US.UTF-8 xargs -0 rm -f)
+unzip $fn
+(find . \( -name '.DS_Store' -o -name '.git*' \) -print0 | xargs -0 rm -f)
 convmv --notest -f UTF-8 -t CP932 -r .
 env LC_ALL=C zip -r $ofn .
