b56781fce3996637d69135038dc2525c75b40151
Takahiro Yoshimura <takahiro_y@monolithworks.co.jp>
2023-06-03 01:29:04 +0900

    Replacing original files by default

--- fixup.sh
+++ fixup.sh
@@ -2,9 +2,12 @@
 export LC_ALL=en_US.UTF-8
 fn=$(realpath "$1")
 ofn=${fn/.z/-fixed.z}
+bfn=${fn/.z/-orig.z}
 tmp=$(mktemp -d)
 cd $tmp
 unzip $fn
 (find . \( -name '.DS_Store' -o -name '.git*' \) -print0 | xargs -0 rm -f)
 convmv --notest -f UTF-8 -t UTF-8 --nfc -r .
 zip -r $ofn .
+mv $fn $bfn
+mv $ofn $fn
